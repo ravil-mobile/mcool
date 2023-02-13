6d71d8f6bec3bac35eadad0550301a9d6fef97b5
ravil <ravil.mobile.com@gmail.com>
2023-02-13 01:58:37 +0100

    Added llvm components to codegen

--- src/CodeGen/CodeGenDriver.cpp
+++ src/CodeGen/CodeGenDriver.cpp
@@ -1,12 +1,21 @@
 #include "CodeGen/CodeGenDriver.h"
-#include <fstream>
+#include <iostream>
 
 namespace mcool::codegen {
 bool CodeGenDriver::run(mcool::AstTree& classes) {
-  std::fstream os(config.outputFile + ".o", std::ios::out);
-  os << "generating source code..." << std::endl;
+  llvmContext = std::make_unique<llvm::LLVMContext>();
+  llvmModule = std::make_unique<llvm::Module>("mcool-module", *llvmContext);
+  llvmBuilder = std::make_unique<llvm::IRBuilder<>>(*llvmContext);
 
-  os.close();
+  std::error_code error{};
+  llvm::raw_fd_ostream fstream(llvm::StringRef(config.outputFile), error);
+  if (not error) {
+    llvmModule->print(fstream, nullptr);
+  } else {
+    std::cerr << "cannot open file: `" << config.outputFile << "`\n";
+    return false;
+  }
+  fstream.close();
   return true;
 }
 } // namespace mcool::codegen
--- src/CodeGen/CodeGenDriver.h
+++ src/CodeGen/CodeGenDriver.h
@@ -2,17 +2,23 @@
 
 #include "Parser/AstTree.h"
 #include "Context.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
 #include <ostream>
 
 namespace mcool::codegen {
 class CodeGenDriver {
   public:
   CodeGenDriver(mcool::Context& context, misc::Config& config)
-      : context(context), config(config) {}
+      : coolContext(context), config(config) {}
   bool run(mcool::AstTree& classes);
 
   private:
-  mcool::Context& context;
+  mcool::Context& coolContext;
   misc::Config& config;
+
+  std::unique_ptr<llvm::LLVMContext> llvmContext;
+  std::unique_ptr<llvm::Module> llvmModule;
+  std::unique_ptr<llvm::IRBuilder<>> llvmBuilder;
 };
 } // namespace mcool::codegen
